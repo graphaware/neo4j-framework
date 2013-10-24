8550fc1b5c51871fb166e79a45f0bc9176b9c1e3
Michal Bachman <git@bachman.cz>
2013-10-24 16:00:11 +0100

    number of shortest paths finder

---
+++ src/main/java/com/graphaware/algo/path/NumberOfShortestPathsFinder.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2013 GraphAware
+ *
+ * This file is part of GraphAware.
+ *
+ * GraphAware is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package com.graphaware.algo.path;
+
+import org.neo4j.graphalgo.impl.path.ShortestPath;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.PathExpander;
+import org.neo4j.helpers.collection.Iterables;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A path finder that finds a given number of shortest paths between two nodes. It is different from {@link ShortestPath}
+ * because it allows to specify the desired number of results. Provided that there are enough paths between the two
+ * nodes in the graph, this path finder will first return all the shortest paths, then all the paths one hop longer,
+ * then two hops longer, etc., until enough paths have been returned.
+ * <p/>
+ * Please note that nodes that are on a path with certain length will not be considered for paths with greater lengths.
+ * For example, given the following graph:
+ * <p/>
+ * (1)->(2)->(3)
+ * (1)->(4)->(5)->(3)
+ * (4)->(2)
+ * <p/>
+ * the shortest path from (1) to (3) is (1)->(2)->(3) and has a length of 2. If more paths are needed, the next path
+ * returned will be (1)->(4)->(5)->(3) with a length of 3. Note that there is another path of length 3:
+ * (1)->(4)->(2)->(3), but it is not returned, since (2)->(3) is contained in a shorter path.
+ */
+public class NumberOfShortestPathsFinder {
+
+    private final int maxDepth;
+    private final int minResultCount;
+    private final PathExpander expander;
+
+    /**
+     * Construct a new path finder.
+     *
+     * @param maxDepth  the maximum depth for the traversal. Returned paths will never have a greater
+     *                  {@link org.neo4j.graphdb.Path#length()} than {@code maxDepth}.
+     * @param noResults the desired number of paths to return, if at all possible. Once reached, search will stop.
+     * @param expander  the {@link org.neo4j.graphdb.PathExpander} to use for deciding which relationships to expand.
+     */
+    public NumberOfShortestPathsFinder(int maxDepth, int noResults, PathExpander expander) {
+        this.maxDepth = maxDepth;
+        this.expander = expander;
+        this.minResultCount = noResults;
+    }
+
+    /**
+     * Find paths between the start and end nodes.
+     *
+     * @param start start node.
+     * @param end   end node.
+     * @return paths between the two nodes, sorted by path lengths.
+     */
+    public List<Path> findPaths(Node start, Node end) {
+        List<Path> result = new LinkedList<Path>();
+
+        //first attempt: classic shortest path
+        result.addAll(Iterables.toList(new ShortestPath(maxDepth, expander).findAllPaths(start, end)));
+
+        //If there are no results, there will never be any. If there are enough, then we just return them:
+        if (result.isEmpty() || result.size() >= minResultCount) {
+            return result;
+        }
+
+        //Now, we have some results, but not enough. All the resulting paths so far must have the same length (they are
+        //the shortest paths after all). We try with longer path length until we have enough:
+        for (int depth = result.get(0).length() + 1; depth <= maxDepth && result.size() < minResultCount; depth++) {
+            result.addAll(Iterables.toList(new ShortestPath(depth, expander, Integer.MAX_VALUE, true).findAllPaths(start, end)));
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/graphaware/algo/path/NumberOfShortestPathsFinderTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2013 GraphAware
+ *
+ * This file is part of GraphAware.
+ *
+ * GraphAware is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package com.graphaware.algo.path;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.graphdb.*;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.neo4j.graphdb.Direction.OUTGOING;
+import static org.neo4j.kernel.Traversal.pathExpanderForAllTypes;
+import static org.neo4j.kernel.Traversal.pathExpanderForTypes;
+
+/**
+ * Unit test for {@link NumberOfShortestPathsFinder}.
+ */
+public class NumberOfShortestPathsFinderTest {
+
+    private enum RelTypes implements RelationshipType {
+        R1, R2
+    }
+
+    private GraphDatabaseService database;
+    private Node one;
+    private Node three;
+
+    /**
+     * Graph:
+     * (1)-[:R2]->(2)-[:R1]->(3)
+     * (1)-[:R1]->(4)-[:R1]->(5)-[:R1]->(3)
+     * (4)-[:R1]->(2)
+     */
+    @Before
+    public void setUp() {
+        database = new TestGraphDatabaseFactory().newImpermanentDatabase();
+
+        Transaction tx = database.beginTx();
+        try {
+            one = database.createNode();
+            Node two = database.createNode();
+            three = database.createNode();
+            Node four = database.createNode();
+            Node five = database.createNode();
+
+            one.createRelationshipTo(two, RelTypes.R2);
+            two.createRelationshipTo(three, RelTypes.R1);
+            one.createRelationshipTo(four, RelTypes.R1);
+            four.createRelationshipTo(five, RelTypes.R1);
+            five.createRelationshipTo(three, RelTypes.R1);
+            four.createRelationshipTo(two, RelTypes.R1);
+
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        database.shutdown();
+    }
+
+    @Test
+    public void noPathsShouldBeReturnedWhenThereIsNoPath() {
+        assertEquals(0, new NumberOfShortestPathsFinder(5, 10, pathExpanderForTypes(RelTypes.R2, OUTGOING)).findPaths(one, three).size());
+    }
+
+    @Test
+    public void noPathsShouldBeFoundWhenTraversalDepthIsTooSmall() {
+        assertEquals(0, new NumberOfShortestPathsFinder(2, 10, pathExpanderForTypes(RelTypes.R1, OUTGOING)).findPaths(one, three).size());
+    }
+
+    @Test
+    public void allShortestPathsShouldBeReturned() {
+        List<Path> paths = new NumberOfShortestPathsFinder(3, 10, pathExpanderForTypes(RelTypes.R1, OUTGOING)).findPaths(one, three);
+        assertEquals(2, paths.size());
+        assertEquals(3, paths.get(0).length());
+        assertEquals(3, paths.get(1).length());
+
+        paths = new NumberOfShortestPathsFinder(3, 10, pathExpanderForAllTypes(OUTGOING)).findPaths(one, three);
+        assertEquals(2, paths.size());
+        assertEquals(2, paths.get(0).length());
+        assertEquals(3, paths.get(1).length());
+    }
+
+    @Test
+    public void shouldLimitPaths() {
+        List<Path> paths = new NumberOfShortestPathsFinder(3, 1, pathExpanderForAllTypes(OUTGOING)).findPaths(one, three);
+        assertEquals(1, paths.size());
+        assertEquals(2, paths.get(0).length());
+    }
+}
