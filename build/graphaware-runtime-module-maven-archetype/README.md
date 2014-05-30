6fa83883f4eadc0069be9d3dc9b1f07d2a595c07
bachmanm <git@bachman.cz>
2014-05-31 00:29:15 +0100

    technical maven modules

---
+++ build/graphaware-runtime-module-maven-archetype/README.md
@@ -0,0 +1,23 @@
+Neo4j GraphAware Runtime Module Maven Archetype
+===============================================
+
+Maven archetype that sets up a project prepared for developing GraphAware Runtime Modules.
+
+Run:
+
+    mvn archetype:generate -DarchetypeGroupId=com.graphaware.neo4j -DarchetypeArtifactId=neo4j-graphaware-runtime-module-maven-archetype -DarchetypeVersion=2.0.3.5
+
+Answer a couple of questions along the way (about your groupId, artifactId and so on), after which you should see
+
+    [INFO] BUILD SUCCESS
+
+License
+-------
+
+Copyright (c) 2014 GraphAware
+
+GraphAware is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ build/graphaware-springmvc-maven-archetype/src/main/resources/archetype-resources/src/main/java/NodeCountApi.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2014 GraphAware
+ *
+ * This file is part of GraphAware.
+ *
+ * GraphAware is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package ${package};
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.helpers.collection.Iterables;
+import org.neo4j.tooling.GlobalGraphOperations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *  Sample REST API for counting all nodes in the database.
+ */
+@Controller
+@RequestMapping("count")
+public class NodeCountApi {
+
+    private final GraphDatabaseService database;
+
+    @Autowired
+    public NodeCountApi(GraphDatabaseService database) {
+        this.database = database;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public long count() {
+        try (Transaction tx = database.beginTx()) {
+            return Iterables.count(GlobalGraphOperations.at(database).getAllNodes());
+        }
+    }
+}
