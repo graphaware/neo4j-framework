342e775c29fa349dbdb3087ad458d085564beb71
Vojta Havlicek <vojta.havlicek@gmail.com>
2014-06-07 08:14:51 +0100

    NeoRank example module structure

--- examples/GraphAware NeoRank/src/main/java/com/graphaware/example/module/NeoRankModule.java
+++ examples/GraphAware NeoRank/src/main/java/com/graphaware/example/module/NeoRankModule.java
@@ -6,10 +6,61 @@
 
 package com.graphaware.example.module;
 
+// GRAPHAWARE FRAMEWORK
+import com.graphaware.common.strategy.InclusionStrategies;
+import com.graphaware.common.strategy.RelationshipInclusionStrategy;
+import com.graphaware.runtime.BaseGraphAwareRuntimeModule;
+import com.graphaware.runtime.config.MinimalRuntimeModuleConfiguration;
+import com.graphaware.runtime.config.RuntimeModuleConfiguration;
+import com.graphaware.tx.event.improved.api.ImprovedTransactionData;
+
+// ENUMS
+import static com.graphaware.example.module.Relationships.*;
+
+// NEO4J
+import java.util.Collections;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+
+
+
 /**
- * A module emulating PageRank-like centrality measure on Neo4j database
+ * A module for a PageRank-like centrality measure on Neo4j database
  * @author Vojtech Havlicek (Graphaware)
  */
-public class NeoRankModule {
+public class NeoRankModule extends BaseGraphAwareRuntimeModule {
+    
+    private final RuntimeModuleConfiguration configuration;
+   
+    public NeoRankModule(String moduleId, GraphDatabaseService database) {    
+       super(moduleId);
+       configuration = new MinimalRuntimeModuleConfiguration(
+                InclusionStrategies.all().with(
+                    new RelationshipInclusionStrategy(){
+                        @Override
+                        public boolean include(Relationship relationship){
+                            return relationship.isType(CASUAL);
+                        }
+                    }
+            ));
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @return 
+     */
+    @Override
+    public RuntimeModuleConfiguration getConfiguration() {
+        return configuration;
+    }
+    
+    
+    @Override
+    public void beforeCommit(ImprovedTransactionData transactionData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
