80eae5f47cb0442d5e022b24158cdf626aa5dcfc
bachmanm <git@bachman.cz>
2014-06-19 15:00:51 +0100

    some comments

--- runtime/src/main/java/com/graphaware/runtime/state/GraphPosition.java
+++ runtime/src/main/java/com/graphaware/runtime/state/GraphPosition.java
@@ -3,7 +3,7 @@ package com.graphaware.runtime.state;
 import org.neo4j.graphdb.GraphDatabaseService;
 
 /**
- *
+ *   //todo Adam's context will be one implementation of this
  */
 public interface GraphPosition<T> {
 
--- runtime/src/main/java/com/graphaware/runtime/state/ModuleContext.java
+++ runtime/src/main/java/com/graphaware/runtime/state/ModuleContext.java
@@ -1,7 +1,7 @@
 package com.graphaware.runtime.state;
 
 /**
- *
+ *       position (where am I + how I got here) plus extra context potentially, such as "the weight I'm carrying"
  */
 public interface ModuleContext<P extends GraphPosition> {
 
