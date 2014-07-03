275b51a55504b14a6bb007ed34b4588a94876cd8
atg103 <atg103@outlook.com>
2014-07-03 16:32:47 +0100

    Adding note to use checked exception in future if module authors must handle this exception, as it says in the JavaDoc.

--- runtime/src/main/java/com/graphaware/runtime/metadata/TimerDrivenModuleContext.java
+++ runtime/src/main/java/com/graphaware/runtime/metadata/TimerDrivenModuleContext.java
@@ -6,9 +6,10 @@ import org.neo4j.graphdb.NotFoundException;
 /**
  * Context of {@link com.graphaware.runtime.module.TimerDrivenModule}s encapsulating the position in the graph and additional
  * data, such as weight carried around for certain iterative algorithms, etc.
- * <p/>
+ * <p>
  * The position in the graph could be a single node, a collection of nodes, a single relationship, a collection of
  * relationships, a cluster of nodes and relationships, etc.
+ * </p>
  *
  * @param <T> type of the position representation.
  */
@@ -17,10 +18,11 @@ public interface TimerDrivenModuleContext<T> {
     /**
      * Find the position in the database.
      *
-     * @param database to find the position in.
-     * @return position.
-     * @throws org.neo4j.graphdb.NotFoundException
-     *          if the position could not be found. Callers must handle this.
+     * @param database The {@link GraphDatabaseService} in which to find the position.
+     * @return A representation of the position.
+     * @throws NotFoundException if the position could not be found. Callers must handle this.
      */
+	//TODO: make this throw a checked exception because we want module authors to handle this situation in their code
     T find(GraphDatabaseService database) throws NotFoundException;
+
 }
