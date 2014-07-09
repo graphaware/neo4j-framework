45469ebea28e4fbea0fa41296d81936ffed98390
bachmanm <git@bachman.cz>
2014-07-09 10:12:21 +0100

    API fix

--- runtime/src/main/java/com/graphaware/runtime/module/BatchSupportingTxDrivenModule.java
+++ runtime/src/main/java/com/graphaware/runtime/module/BatchSupportingTxDrivenModule.java
@@ -4,8 +4,12 @@ import com.graphaware.tx.event.batch.api.TransactionSimulatingBatchInserter;
 
 /**
  * {@link TxDrivenModule} that can be backed by a {@link TransactionSimulatingBatchInserter} (on top of {@link org.neo4j.graphdb.GraphDatabaseService}).
+ *
+ * @param <T> The type of a state object that the module can use to
+ *            pass information from the {@link #beforeCommit(com.graphaware.tx.event.improved.api.ImprovedTransactionData)}
+ *            method to the {@link #afterCommit(Object)} method.
  */
-public interface BatchSupportingTxDrivenModule extends TxDrivenModule {
+public interface BatchSupportingTxDrivenModule<T> extends TxDrivenModule<T> {
 
     /**
      * Initialize this module. This method must bring the module to a state equivalent to a state of the same module that
