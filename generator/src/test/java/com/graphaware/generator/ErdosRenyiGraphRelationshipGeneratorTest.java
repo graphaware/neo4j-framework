1c9f66fa366982fde1ada67d2c9f590311ae89af
Vojta Havlicek <vojta.havlicek@gmail.com>
2014-07-12 14:14:15 +0100

    ER test - profile?

---
+++ generator/src/test/java/com/graphaware/generator/ErdosRenyiGraphRelationshipGeneratorTest.java
@@ -0,0 +1,19 @@
+package com.graphaware.generator;
+
+import com.graphaware.generator.config.ErdosRenyiConfig;
+import com.graphaware.generator.relationship.ErdosRenyiGraphRelationshipGenerator;
+import junit.framework.TestCase;
+
+public class ErdosRenyiGraphRelationshipGeneratorTest extends TestCase {
+
+    public void testDoGenerateEdges() {
+        // EXPERIMENTAL:
+        ErdosRenyiConfig config = new ErdosRenyiConfig(1000000, 5000000); // Works fine up to ~ 1000000
+        ErdosRenyiGraphRelationshipGenerator er = new ErdosRenyiGraphRelationshipGenerator(config);
+        System.out.println(er.generateEdges().size());
+
+//        ErdosRenyiConfig config;
+//        config = new ErdosRenyiConfig(20, 190);
+//        System.out.println(er.generateEdges(config));
+    }
+}
