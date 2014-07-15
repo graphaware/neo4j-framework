dde08b9ad7d67ce074b27e7951e871455dd1e827
atg103 <atg103@outlook.com>
2014-07-15 12:02:22 +0100

    Adding methods to transaction monitor.

---
+++ runtime/src/main/java/com/graphaware/runtime/monitor/Throughput.java
@@ -0,0 +1,10 @@
+package com.graphaware.runtime.monitor;
+
+/**
+ * Encapsulates information about the rate of transaction throughput as monitored by the Runtime.
+ */
+public class Throughput {
+
+	// no code here yet, WIP
+
+}
