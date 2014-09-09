3f7cf92316e195e04990dc289856c8f6448dfa24
bachmanm <git@bachman.cz>
2014-09-09 14:08:32 +0000

    prepare for another release

--- common/src/main/java/com/graphaware/common/ping/StatsCollector.java
+++ common/src/main/java/com/graphaware/common/ping/StatsCollector.java
@@ -5,7 +5,7 @@ package com.graphaware.common.ping;
  */
 public interface StatsCollector {
 
-    public static final String VERSION = "2.1.4.16";
+    public static final String VERSION = "2.1.4.17";
 
     /**
      * Report framework start (server mode).
