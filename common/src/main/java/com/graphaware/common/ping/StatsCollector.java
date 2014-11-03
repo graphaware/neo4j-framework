4872b9b410c8fff52f73e5a05c4b89f5703bfe53
bachmanm <git@bachman.cz>
2014-11-03 23:10:14 +0100

    prepare for next release

--- common/src/main/java/com/graphaware/common/ping/StatsCollector.java
+++ common/src/main/java/com/graphaware/common/ping/StatsCollector.java
@@ -5,7 +5,7 @@ package com.graphaware.common.ping;
  */
 public interface StatsCollector {
 
-    public static final String VERSION = "2.1.5.22";
+    public static final String VERSION = "2.1.5.23";
 
     /**
      * Report framework start (server mode).
--- tx-executor/README.md
+++ tx-executor/README.md
@@ -13,7 +13,7 @@ Add the following snippet to your pom.xml:
 <dependency>
     <groupId>com.graphaware.neo4j</groupId>
     <artifactId>tx-executor</artifactId>
-    <version>2.1.5.22</version>
+    <version>2.1.5.23</version>
 </dependency>
 ```
 
