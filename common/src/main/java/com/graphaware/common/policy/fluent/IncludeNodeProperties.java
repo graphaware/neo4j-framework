98df177048fc842a2f16a13595bbcf45b41b5f67
bachmanm <git@bachman.cz>
2015-02-16 21:46:46 +0000

    property inclusion policies with a fluent interface

---
+++ common/src/main/java/com/graphaware/common/policy/fluent/IncludeNodeProperties.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 GraphAware
+ *
+ * This file is part of GraphAware.
+ *
+ * GraphAware is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package com.graphaware.common.policy.fluent;
+
+import com.graphaware.common.policy.NodePropertyInclusionPolicy;
+import org.neo4j.graphdb.Node;
+
+/**
+ * An implementation of {@link NodePropertyInclusionPolicy} that is entirely configurable using its fluent interface.
+ */
+public class IncludeNodeProperties extends BaseIncludeProperties<IncludeNodeProperties, Node> implements NodePropertyInclusionPolicy {
+
+    public static IncludeNodeProperties all() {
+        return new IncludeNodeProperties(null);
+    }
+
+    protected IncludeNodeProperties(String key) {
+        super(key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected IncludeNodeProperties newInstance(String key) {
+        return new IncludeNodeProperties(key);
+    }
+}
