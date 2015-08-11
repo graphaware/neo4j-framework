c01776c66c2b48f8d6186ac387a1cca17d264611
Michal Bachman <michal@graphaware.com>
2015-08-11 12:34:08 +0200

    upgrade of Neo4j 2.2.4

--- common/src/main/java/com/graphaware/common/ping/StatsCollector.java
+++ common/src/main/java/com/graphaware/common/ping/StatsCollector.java
@@ -21,7 +21,7 @@ package com.graphaware.common.ping;
  */
 public interface StatsCollector {
 
-    public static final String VERSION = "2.2.4.33";
+    public static final String VERSION = "2.2.4.34";
 
     /**
      * Report framework start (server mode).
