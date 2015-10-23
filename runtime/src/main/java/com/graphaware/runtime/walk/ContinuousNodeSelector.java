9eccb488a82e98f4d3e9286f1b97530a44c03a85
Michal Bachman <michal@graphaware.com>
2015-10-23 08:41:39 -0700

    Merge branch 'master' into 2.3-SNAPSHOT
    # Conflicts:
    #	README.md
    #	api/pom.xml
    #	build/graphaware-framework-server-community/pom.xml
    #	build/graphaware-framework-server-enterprise/pom.xml
    #	build/graphaware-framework/pom.xml
    #	build/pom.xml
    #	common/README.md
    #	common/pom.xml
    #	common/src/main/java/com/graphaware/common/ping/StatsCollector.java
    #	examples/change-logger/pom.xml
    #	examples/friendship-strength-counter-module/pom.xml
    #	examples/friendship-strength-counter/pom.xml
    #	examples/integration-testing/pom.xml
    #	examples/node-counter/pom.xml
    #	examples/performance-testing/pom.xml
    #	parent-pom/framework/pom.xml
    #	parent-pom/module/pom.xml
    #	pom.xml
    #	runtime-api/pom.xml
    #	runtime/README.md
    #	runtime/pom.xml
    #	server/README.md
    #	server/pom.xml
    #	server/server-common/pom.xml
    #	server/server-community/pom.xml
    #	server/server-enterprise/pom.xml
    #	tests/README.md
    #	tests/pom.xml
    #	tx-api/README.md
    #	tx-api/pom.xml
    #	tx-api/src/test/java/com/graphaware/tx/event/improved/FilteredLazyTransactionDataIntegrationTest.java
    #	tx-api/src/test/java/com/graphaware/tx/event/improved/LazyTransactionDataComprehensiveTest.java
    #	tx-executor/README.md
    #	tx-executor/pom.xml
    #	writer-api/pom.xml
    #	writer/pom.xml

--- runtime/src/main/java/com/graphaware/runtime/walk/ContinuousNodeSelector.java
+++ runtime/src/main/java/com/graphaware/runtime/walk/ContinuousNodeSelector.java
@@ -22,7 +22,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.NotFoundException;
 import org.neo4j.kernel.GraphDatabaseAPI;
-import org.neo4j.kernel.impl.transaction.state.NeoStoreSupplier;
+import org.neo4j.kernel.impl.store.NeoStores;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -108,7 +108,7 @@ public class ContinuousNodeSelector implements NodeSelector {
     }
 
     private long nextId(GraphDatabaseService database) {
-        long highestId = ((GraphDatabaseAPI) database).getDependencyResolver().resolveDependency(NeoStoreSupplier.class).get().getNodeStore().getHighestPossibleIdInUse();
+        long highestId = ((GraphDatabaseAPI) database).getDependencyResolver().resolveDependency(NeoStores.class).getNodeStore().getHighestPossibleIdInUse();
         long nextId = lastId.incrementAndGet();
 
         if (nextId > highestId) {
