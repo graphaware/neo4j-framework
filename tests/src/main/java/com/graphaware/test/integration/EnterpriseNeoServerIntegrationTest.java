7a8f00bda4d4eeb7c0466a89d35a002556874ae2
Michal Bachman <michal@graphaware.com>
2015-10-29 02:25:00 -0600

    extra test classes for convenience

---
+++ tests/src/main/java/com/graphaware/test/integration/EnterpriseNeoServerIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.graphaware.test.integration;
+
+/**
+ * Community {@link NeoServerIntegrationTest}.
+ */
+public class EnterpriseNeoServerIntegrationTest extends NeoServerIntegrationTest {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected NeoTestServer neoTestServer(String neo4jConfigFile, String neo4jServerConfigFile) {
+        return new CommunityNeoTestServer(neo4jConfigFile, neo4jServerConfigFile);
+    }
+}
