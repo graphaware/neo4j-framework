ddc3348123da028d7208b753e755a70b9401327b
Michal Bachman <michal@graphaware.com>
2015-12-06 15:44:01 +0000

    an extra test suite for adding tests that make sure event handlers behave correctly when addiotional modifications are performed within them

---
+++ tx-api/src/test/java/com/graphaware/tx/event/improved/AdditionalModificationsIntegrationTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2013-2015 GraphAware
+ *
+ * This file is part of the GraphAware Framework.
+ *
+ * GraphAware Framework is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package com.graphaware.tx.event.improved;
+
+import com.graphaware.test.data.CypherPopulator;
+import com.graphaware.test.data.DatabasePopulator;
+import com.graphaware.test.integration.DatabaseIntegrationTest;
+import com.graphaware.tx.event.improved.api.ImprovedTransactionData;
+import com.graphaware.tx.event.improved.api.LazyTransactionData;
+import org.junit.Test;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.DynamicRelationshipType;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.event.TransactionData;
+import org.neo4j.graphdb.event.TransactionEventHandler;
+
+import static org.junit.Assert.assertEquals;
+
+public class AdditionalModificationsIntegrationTest extends DatabaseIntegrationTest {
+
+
+    @Override
+    protected DatabasePopulator databasePopulator() {
+        return new CypherPopulator() {
+            @Override
+            protected String[] statementGroups() {
+                return new String[]{"CREATE " +
+                        "(m:Person {name:'Michal'})," +
+                        "(l:City {name:'London'})," +
+                        "(p:City {name:'Prague'})," +
+                        "(m)-[:LIVES_IN]->(p)," +
+                        "(m)-[:LIVES_IN]->(l)" +
+                        ""};
+            }
+        };
+    }
+
+    @Test
+    public void additionalCreatesShouldNotImpactTxData() {
+        getDatabase().registerTransactionEventHandler(new TransactionEventHandler.Adapter<Void>(){
+
+            @Override
+            public Void beforeCommit(TransactionData data) throws Exception {
+                ImprovedTransactionData transactionData = new LazyTransactionData(data);
+
+                assertEquals(1, transactionData.getAllCreatedNodes().size());
+
+                for (Node node : transactionData.getAllCreatedNodes()) {
+                    Node unknownCity = node.getGraphDatabase().createNode(DynamicLabel.label("City"));
+                    node.createRelationshipTo(unknownCity, DynamicRelationshipType.withName("LIVES_IN"));
+                }
+
+                assertEquals(1, transactionData.getAllCreatedNodes().size());
+
+                return null;
+            }
+        });
+    }
+
+
+}
