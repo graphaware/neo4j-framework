648ea4c47308c0460c981336fb08ea74aa602f36
Michal Bachman <michal@graphaware.com>
2017-04-17 20:51:27 +0400

    make it compile for 3.2

--- runtime/src/test/java/com/graphaware/runtime/OtherRuntimeTests.java
+++ runtime/src/test/java/com/graphaware/runtime/OtherRuntimeTests.java
@@ -30,8 +30,8 @@ import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.kernel.configuration.Settings;
 import org.neo4j.shell.ShellSettings;
-import org.neo4j.test.RepeatRule;
 import org.neo4j.test.TestGraphDatabaseFactory;
+import org.neo4j.test.rule.RepeatRule;
 
 import java.util.Random;
 
