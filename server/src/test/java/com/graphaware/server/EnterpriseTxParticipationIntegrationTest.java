994e70834ce2168f81a08304bc827301888eec74
Michal Bachman <michal@graphaware.com>
2018-05-18 19:17:59 +0400

    enable disabled tests

--- server/src/test/java/com/graphaware/server/EnterpriseTxParticipationIntegrationTest.java
+++ server/src/test/java/com/graphaware/server/EnterpriseTxParticipationIntegrationTest.java
@@ -79,6 +79,6 @@ public class EnterpriseTxParticipationIntegrationTest extends GraphAwareIntegrat
         //Commit transaction over transactional endpoint
         httpClient.post(commitUrl, HttpStatus.SC_OK);
 
-//        httpClient.post(baseUrl() + "/resttest/assertSameGraph", "{\"cypher\": \"CREATE (m:Person {name:'Michal'})-[:TEST]->(d:Person {name:'Daniela'})\"}", HttpStatus.SC_OK);
+        httpClient.post(baseUrl() + "/resttest/assertSameGraph", "{\"cypher\": \"CREATE (m:Person {name:'Michal'})-[:TEST]->(d:Person {name:'Daniela'})\"}", HttpStatus.SC_OK);
     }
 }
